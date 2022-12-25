118a0df51c11a4943ebd8702754da1cdae6445d7
Vivek jha <vivekjha3080@gmail.com>
2022-12-25 13:12:57 +0530

    signup login chat

--- Prega-X/src/App.jsx
+++ Prega-X/src/App.jsx
@@ -3,15 +3,23 @@ import { Route, Routes } from 'react-router';
 import './App.css';
 import Trimester from './Components/Trimester/Trimester';
 import TrimesterList from './Components/TrimesterList/TrimesterList';
+import Todo from './Components/todo/todo'
+import Chatbox from './Components/chat/Chatbox'
 
 import { trimester } from './data';
+import LoginForm from './Components/login/Loginform';
+import SignupForm from './Components/login/Signup';
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path="/trimester" element={<TrimesterList/>} />
-        <Route path="/trimester/:id" element={<Trimester data={trimester}/>} />
+        <Route path="/trimester" element={<TrimesterList />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/chat" element={<Chatbox></Chatbox>} />
+        <Route path="/trimester/:id" element={<Trimester data={trimester} />} />
+        <Route path="/login" element={<LoginForm></LoginForm>} />
+        <Route path="/signup" element={<SignupForm></SignupForm>} />
       </Routes>
     </div>
   )
